7bbd5f108fc0cf8e64fbd1c52aa360d3f69226f5
P. Christeas <p_christ@hol.gr>
2010-05-12 01:53:04 +0300

    sandbox: use cmdline params in extaparams.py
    With this, extparams.py can be instructed to behave differently, no need
    to comment-out parts.

--- sandbox/extparams.py
+++ sandbox/extparams.py
@@ -5,10 +5,35 @@ import os
 from distutils.util import get_platform
 import sys
 
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-q", "--quiet",
+                  action="store_false", dest="verbose", default=True,
+                  help="don't print status messages to stdout")
+
+parser.add_option("-t", None,
+                  action="store_true", dest="old_proto", default=False,
+                  help="Uses the old (installed) library")
+
+parser.add_option("-n", "--num-reps", dest="num_reps", 
+		  help="Number of repetitions for stress test",)
+
+parser.add_option("-N", "--no-stress",
+                  action="store_false", dest="stress", default=True,
+                  help="Disable the stress test")
+
+parser.add_option("-R", "--no-regulars",
+                  action="store_false", dest="regulars", default=True,
+                  help="Disable the regular tests")
+
+
+(options, args) = parser.parse_args()
+
 # Insert the distutils build directory into the path, if it exists.
 platlib = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'build',
                        'lib.%s-%s' % (get_platform(), sys.version[0:3]))
-if os.path.exists(platlib):
+if os.path.exists(platlib) and not options.old_proto:
     print "Get psycopg2 from ", platlib
     sys.path.insert(0, platlib)
 
@@ -33,12 +58,33 @@ conn = psycopg2.connect(dsn)
 print 'Connection established'
 cr = conn.cursor()
 
-cr.execute('SELECT %s',(1,))
-print cr.fetchall()
+if options.regulars:
+    print "Simple select:"
+    cr.execute('SELECT %s',(1,))
+    print cr.fetchall()
+    
+    print "Single cmd with semicolon:"
+    cr.execute("SELECT %s;   	", (1,))
+    print cr.fetchall()
+    
+    try:
+	print "Multi cmds:"
+	cr.execute("SELECT '%s'; SELECT '2' ;", (1,))
+	print cr.fetchall()
+    except psycopg2.ProgrammingError, e:
+	print "Programming error:", e
+	pass
+    
+    print "Several types:"
+    cr.execute('SELECT %s,%s,%s,%s; ', (1, 1L, -1, 'str1', None))
+    print "Result:", cr.fetchall()
+    
+    
 
-ran = range(1, 1500)
-qry = 'SELECT (' + ', '.join([ '%s' for x in ran]) + ');'
-for i in range(1, 1000):
-    cr.execute(qry, ran)
-    res = cr.fetchall()
+if options.stress:
+    ran = range(1, 1500)
+    qry = 'SELECT (' + ', '.join([ '%s' for x in ran]) + ');'
+    for i in range(1, 1000):
+	cr.execute(qry, ran)
+	res = cr.fetchall()
 
