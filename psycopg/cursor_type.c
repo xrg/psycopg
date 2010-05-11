fbb496042c0ad2ae9183f78c2dce06b99a8b1368
P. Christeas <p_christ@hol.gr>
2010-05-12 01:52:08 +0300

    exec params: detect and bailout on multiple queries.
    For the moment, the code will just except when multiple queries are fed
    to cr.execute(). Note that it will still misinterpret a quoted \';\' as
    a semicolon. Better play it safe.

--- psycopg/cursor_type.c
+++ psycopg/cursor_type.c
@@ -383,6 +383,7 @@ _mogrify_execparams(PyObject *var, PyObject *fmt, connectionObject *conn,
     char *d, *c, *c_begin;
     int kind = 0, nParams = 0, index = 0, oindex = 0;
     int i, ri;
+    int is_multi = 0;
     
     /* The command buffer. We avoid a python object, because we don't really
     want one. We allocate cmd_alloc bytes, when our estimates need cmdlen.
@@ -465,6 +466,14 @@ _mogrify_execparams(PyObject *var, PyObject *fmt, connectionObject *conn,
             c+=2;
         }
         else {
+	    if ( *c == ';'){
+		Dprintf("locate semicolon");
+		is_multi ++;
+	    }
+	    else if (is_multi && ! isspace(*c)){
+		Dprintf("This algo cannot handle multiple queries!");
+		return -2;
+	    }
             c++;
             cmdlen++;
         }
@@ -718,6 +727,8 @@ _psyco_curs_execute(cursorObject *self,
             goto fail;
         else if (mres == -2){ /* retry the old way */
             Dprintf("Fallback to the old pq_execute code");
+            psyco_set_error(ProgrammingError, (PyObject*) self,
+                        "Old path not supported", NULL, NULL);
             /* TODO */
             goto fail;
         }
